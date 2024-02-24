a367c3ac4262cb74c2b91380f025b93bb81f71f6
Magua William <williammagua@gmail.com>
2024-02-24 12:47:00 +0300

    passed the remove array function test and edited the .spec.js file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    const result= [];
+    array.forEach(function(item) {
+        if(!items.includes(item)) {
+            result.push(item);
+            }
+    });
+    return result;
+    // const newArr = [];
+    // for(let item of array) {
+    //     if (!items.includes(item)){
+    //         newArr.push(item);
+    //         }
+    // }
+    // return newArr;
 };
 
 // Do not edit below this line
