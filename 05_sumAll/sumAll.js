9c7d2edc3e772ef9deebc912529c9f01f3257b25
Magwargit config --global user.email williammagua@gmail.com <111179088+Magwar@users.noreply.github.com>
2024-02-25 16:54:28 +0300

    Passed the sumAll function test and edited the spec.js file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0 ) {
+        return 'ERROR!'
+    }
+    let total = 0;
+    for(let num = start; num <= end; num++){
+        total += num;
+    }
+    return total;
 };
 
 // Do not edit below this line
