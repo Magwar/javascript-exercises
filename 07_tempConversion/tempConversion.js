9580d7b4843ad31658ca2815f77b17c0fab9b81a
Magua William <williammagua@gmail.com>
2024-03-11 14:27:25 +0300

    Created the required functions to the task making sure they pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * 5/9;
+  if (!Number.isInteger(convertedTemp)) {
+      return parseFloat(convertedTemp.toFixed(1));
+  }else {
+      return parseFloat(convertedTemp);
+  }
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = (temp * 9/5) + 32;
+  if (!Number.isInteger(fahrenheitTemp)) {
+    return parseFloat(fahrenheitTemp.toFixed(1));
+  }else{
+    return  parseFloat(fahrenheitTemp);
+  }
 };
 
 // Do not edit below this line
